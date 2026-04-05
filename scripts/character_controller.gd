ab2dfc0eb212bec3c95b337ccf6bd9600f8fc3fb
lenoa7vo-png <lenoa7vo@gmail.com>
2026-04-05 14:58:18 +0800

    character weapon

--- scripts/character_controller.gd
+++ scripts/character_controller.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 #@export var charVis:Sprite2D
+@export var weapon:  = preload("res://scenes/weapon.tscn")
 @export var muzzle:Node2D
 @export var speed:float = 300
 var move:Vector2
@@ -22,6 +23,14 @@ func _physics_process(delta: float) -> void:
 	count += delta
 	get_input()
 	var xMove:float = moveDir * speed
+	
+	if fire and count >= fireRate:
+		count = 0
+		var bullet:Weapon1 = weapon.instantiate()
+		bullet.position = muzzle.position
+		add_child(bullet)
+		
+		#print_debug(bullet.position)
 	
 	if xMove: #apply horizontal movement
 		velocity.x = xMove

b7113b2dda4acb39ae17e19824c4d98e96fa7c16
0neAnd0nlyKen <kendrickraphael@gmail.com>
2026-04-10 10:47:14 +0800

    new shielder enemy type

--- scripts/enemy/shielder_enemy.gd
+++ scripts/enemy/shielder_enemy.gd
@@ -2,31 +2,39 @@ extends Enemy
 
 @export var shieldNode: Area2D
 @export var shieldCol: CollisionShape2D
-
+@export var shieldSprite: Sprite2D
 func _ready() -> void:
 	setup_shield()
+	# Override defaults for heavy enemy
+	max_health = 40
+	health = max_health
+	enemy_score = 250
+	base_speed = 15  # Slower
+	super()
+
+func move_enemy(_delta: float) -> void:
+	# Heavy enemies move straight down but slower
+	velocity.y = base_speed
 
 func setup_shield():
 	var viewport_rect = get_viewport_rect()
 	var screen_width = viewport_rect.size.x
 	
-	shieldNode.position.x = screen_width / 2
+	var shield_width = screen_width
+	var shield_height = 16
+	
+	# Set collision shape size
+	if shieldCol.shape is RectangleShape2D:
+		shieldCol.shape.size = Vector2(shield_width, shield_height)
+	
+	# Set sprite size to match
+	if shieldSprite.texture:
+		var texture_size = shieldSprite.texture.get_size()
+		shieldSprite.scale = Vector2(shield_width / texture_size.x, shield_height / texture_size.y)
+	
+	#shieldNode.global_position.x = screen_width
+	shieldNode.global_position.x = 0
 
-	var collision_shape: RectangleShape2D = shieldCol.shape
-	collision_shape.size.x = screen_width
-
-	# Match Sprite2D size to CollisionShape2D
-	var sprite: Sprite2D = $ShieldNode/Sprite2D
-	if sprite:
-		# Create a procedural texture for the shield
-		var image = Image.create(int(collision_shape.size.x), int(collision_shape.size.y), false, Image.FORMAT_RGBA8)
-		image.fill(Color(0, 1, 0, 0.5))  # Semi-transparent green shield
-		
-		var texture = ImageTexture.create_from_image(image)
-		sprite.texture = texture
-		
-		# Reset scale since we're using the actual size
-		sprite.scale = Vector2.ONE
 
 func _on_shield_node_area_entered(area: Area2D) -> void:
 	if area is FriendlyWeapon:

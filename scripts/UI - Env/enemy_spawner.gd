b16109a6450b270d304e5553305c3f7e429d5768
0neAnd0nlyKen <kendrickraphael@gmail.com>
2026-04-11 23:06:52 +0800

    fix bug spawner and man menu ui

--- scripts/UI - Env/enemy_spawner.gd
+++ scripts/UI - Env/enemy_spawner.gd
@@ -3,9 +3,10 @@ class_name EnemySpawner
 
 @export var spawnAnchor:Node2D
 @export var maxEnemies: int = 10
-@export var spawnRate: float = 2.0
+@export var spawnRate: float = 3.0
 @export var world: World
 @export var enemy_types: Array[EnemyType]
+@export var enemy_hp_multi: float = 1
 
 
 signal difficulty_increased()
@@ -14,7 +15,7 @@ var _spawn_pool: Array[EnemyType] = []
 var spawnTimer: float = 0.0
 var wave_counter: int = 0  # Track spawning waves for variety
 var difficulty_level: int = 0
-
+var enemies: int = 0
 
 func _ready() -> void:
 	if spawnAnchor == null:
@@ -40,15 +41,17 @@ func _build_weighted_spawn_pool():
 
 func _process(delta: float) -> void:
 	var spawnPoint:Vector2 = Vector2(randf_range(-400, 400), spawnAnchor.position.y)
-	spawnTimer += delta
 	
-	if spawnTimer >= spawnRate:
-		spawnTimer = 0.0
-		if get_child_count() < maxEnemies:
+	spawnTimer += delta
+	if spawnTimer >= spawnRate and enemies < maxEnemies:
+			spawnTimer = 0.0
+			enemies += 1
 			var newEnemy = spawn_enemy_variation(spawnPoint)
 			if newEnemy:
+				newEnemy.health *= enemy_hp_multi
 				if world != null and world.has_method("_on_enemy_defeated"):
 					newEnemy.enemy_defeated.connect(world._on_enemy_defeated)
+					newEnemy.enemy_defeated.connect(_on_enemy_defeated)
 				if world != null and world.has_method("_on_enemy_landed"):
 					newEnemy.enemy_landed.connect(world._on_enemy_landed)
 
@@ -64,6 +67,13 @@ func spawn_enemy_variation(spawn_point: Vector2) -> Enemy:
 
 func _on_difficulty_increased() -> void:
 	difficulty_level += 1
-	spawnRate = max(0.5, spawnRate - 0.3)  # Make spawning more frequent
-	maxEnemies += 1  # Allow more enemies on screen
+	spawnRate = max(0.5, spawnRate * 0.9)  # Make spawning more frequent
+	maxEnemies *= 1  # Allow more enemies on screen
 	_build_weighted_spawn_pool()  # Update spawn pool with new difficulty
+	enemy_hp_multi *= 1
+
+func _on_enemy_defeated(enemyScore: float  ) -> void:
+	print_debug("spawn timer ",spawnTimer," spawnrate ", spawnRate, " max enemy ", maxEnemies)
+	print_debug("enemies before ", enemies)
+	enemies -= 1
+	print_debug("enemies after ", enemies)
